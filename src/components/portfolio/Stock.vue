37ad9b3ae5637497d2e0533b9779b8ef1cb78b74
Nikola Obradovic <obradovicnik01@gmail.com>
2019-10-01 18:06:17 +0200

    Added some markup

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>The stock - portfolio component</h1>
+  </div>
+</template>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>The Stocks component</h1>
+  </div>
+</template>
